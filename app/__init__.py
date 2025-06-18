aa56e536c7694c54889c4dcfd4ee7c0b93eb093e
Daniel <Daniel.halaas@gmail.com>
2025-06-18 17:42:50 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
+from .config import DevelopmentConfig, ProductionConfig, TestingConfig
 import os
 
 
-def create_app():
+def create_app(config_name='development'):
     load_dotenv()
-    
     app = Flask(__name__)
     CORS(app, supports_credentials=True, origins=[os.getenv("FRONTEND_URL", "http://localhost:5173")])
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev-key')
+
+    if config_name == "production":
+        app.config.from_object(ProductionConfig)
+    elif config_name == "testing":
+        app.config.from_object(TestingConfig)
+    else:
+        app.config.from_object(DevelopmentConfig)   
 
     return app
