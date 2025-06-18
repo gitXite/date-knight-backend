756d44e5752ed5566e1e9209317bb316fa4b4784
Daniel <Daniel.halaas@gmail.com>
2025-06-18 17:13:17 +0200

    create_app() config

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
 
 def create_app():
+    load_dotenv()
+    
     app = Flask(__name__)
-    # config here
+    CORS(app, supports_credentials=True, origins=[os.getenv("FRONTEND_URL", "http://localhost:5173")])
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev-key')
 
     return app
