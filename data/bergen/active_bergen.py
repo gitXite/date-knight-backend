971268bde26ce44c1e246b8ddb03d36fc1603d01
Daniel <Daniel.halaas@gmail.com>
2025-06-18 16:53:10 +0200

    Update active_bergen.py

--- data/bergen/active_bergen.py
+++ data/bergen/active_bergen.py
@@ -1,1 +1,81 @@
-
+active_bergen = {
+    "Bar games": {
+        "Bar 3": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Inside Rock Cafe": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Park N": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ]
+    },
+    "Run/hike": {
+        "Stoltzekleiven": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Ulrikken": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Vidden": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ], 
+        "Fløyen": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Løvstakken": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Store Lungegårdsvannet": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ]
+    }, 
+    "Sauna": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Dance lessons": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Sup/kayak": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Bouldering": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Padel": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Squash": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+}
