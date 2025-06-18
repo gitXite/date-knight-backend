76942b31bebfac8238c277e83a424eaec9a55ee2
Daniel <Daniel.halaas@gmail.com>
2025-06-18 16:45:35 +0200

    Update creative_bergen.py

--- data/bergen/creative_bergen.py
+++ data/bergen/creative_bergen.py
@@ -1,1 +1,58 @@
-
+creative_bergen = {
+    "Homemade dinner": {
+        "Marry me chicken": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Sweet & sour chicken on cashew bed": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Pasta carbonara": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Steak": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ]
+    },
+    "Jigsaw": [
+        {"Time": ""},
+        {"Cost": ""}
+    ],
+    "Stand up": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Paint'n Sip": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Pottery class": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Cooking class": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Dance lessons": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Art exhibit": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+}
